434b80e284724c7165afce7b50b03952c677f0aa
manishkharthik <manishkharthik0630@gmail.com>
2025-08-21 22:32:33 +0800

    Replaced Duke.java with Salah.java, and introduced unique greeting and exit

---
+++ src/main/java/Salah.java
@@ -0,0 +1,24 @@
+public class Salah {
+    public static void main(String[] args) {
+        String salah = 
+        """
+                       /^\\
+                      /___\\
+                     ( o o )
+                     /  ^  \\
+              ____ _/|  |  |\\_ ____
+                   / |  |  | \\
+                  /  |  |  |  \\
+                 /___|  |  |___\\
+                      \\  |  /
+                       \\ | /
+                    ____\\|/____
+                   /____/ \\____\\
+                       /_____\\
+        """ ;
+        
+        System.out.println("Hi, I'm Mohammed Salah, The Egyptian King\n" + salah);
+        System.out.println("How may I assist you today?");
+        System.out.println("Hope you have a great day! Remember, you'll never walk alone :))");
+    }
+}
