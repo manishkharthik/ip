3b09421986c4d15c799afe00769cc7e3a59a16f7
manishkharthik <manishkharthik0630@gmail.com>
2025-08-21 23:04:25 +0800

    Imported scanner for user inputs and introduced echo feature

--- src/main/java/Salah.java
+++ src/main/java/Salah.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Salah {
     public static void main(String[] args) {
         String salah = 
@@ -19,6 +21,18 @@ public class Salah {
         
         System.out.println("Hi, I'm Mohammed Salah, The Egyptian King\n" + salah);
         System.out.println("How may I assist you today?");
-        System.out.println("Hope you have a great day! Remember, you'll never walk alone :))");
+        Scanner scan = new Scanner(System.in);
+        while (true) { 
+            String input = scan.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Hope you have a great day! Remember, you'll never walk alone :))");
+                break;
+            } else {
+                System.out.println("-------------------------------------------------");
+                System.out.println(input);
+                System.out.println("-------------------------------------------------");
+            }
+        }
+        scan.close();
     }
 }
