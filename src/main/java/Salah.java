f973112a68a703aed26db77b32e86e29b2ef6283
manishkharthik <manishkharthik0630@gmail.com>
2025-08-21 23:16:46 +0800

    Introduce userTasks as ArrayList, implement add to and display userTasks

--- src/main/java/Salah.java
+++ src/main/java/Salah.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Salah {
+    public static ArrayList<String> userTasks = new ArrayList<>();
     public static void main(String[] args) {
         String salah = 
         """
@@ -25,11 +27,22 @@ public class Salah {
         while (true) { 
             String input = scan.nextLine();
             if (input.equals("bye")) {
-                System.out.println("Hope you have a great day! Remember, you'll never walk alone :))");
-                break;
-            } else {
                 System.out.println("-------------------------------------------------");
-                System.out.println(input);
+                System.out.println("Hope you have a great day! Remember, you'll never walk alone :))");
+                System.out.println("-------------------------------------------------");
+                break;
+            } else if (input.equals("list")) {
+                System.out.println("-------------------------------------------------");
+                int num = 1;
+                for (String task : userTasks) {
+                    System.out.println(num + ". " + task);
+                    num++;
+                }
+                System.out.println("-------------------------------------------------");
+            } else {
+                userTasks.add(input);
+                System.out.println("-------------------------------------------------");
+                System.out.println("added: " + input);
                 System.out.println("-------------------------------------------------");
             }
         }
